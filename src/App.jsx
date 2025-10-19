571bd78f35f8da05f893d2621564c81d42d43a01
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-10-19 23:20:05 +0545

    feat: add Create page component; implement form for blog creation and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import SinglePage from "./pages/SinglePage"
+import Create from "./pages/Create"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog/:id" element={<SinglePage />} />
+          <Route path="/create" element={<Create />} />
         </Routes>
       </BrowserRouter>
     </>
